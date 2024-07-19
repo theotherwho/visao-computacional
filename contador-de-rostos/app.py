25e28db360dad025e9d2ef8f871993ebce7cf344
theotherswho <cristianomc@gmail.com>
2024-07-19 00:05:38 -0300

    ENTREGA REALIZADA

--- contador-de-rostos/app.py
+++ contador-de-rostos/app.py
@@ -1,12 +1,19 @@
 import cv2
 import numpy as np
-#CRISTIANO MAGALHAES CARDOSO
-#FUNDAMENTOS DE INTELIGENCIA ARTIFICIAL
-#18/07/2024
+# CRISTIANO MAGALHAES CARDOSO
+# FUNDAMENTOS DE INTELIGENCIA ARTIFICIAL
+# 18/07/2024
 def main():
     video = cv2.VideoCapture('contador-de-rostos/pessoas-skiando.mp4')
     contador = 0
     liberado = True  # Modificado para começar como True, permitindo a primeira contagem
+
+    # Inicializa o subtrator de fundo
+    backSub = cv2.createBackgroundSubtractorMOG2()
+
+    # Definindo a região de interesse (ROI)
+    x, y, w, h = 500, 430, 30, 290
+    total_pixels = w * h  # Calcula o número de pixels na área de interesse
 
     while True:
         ret, img = video.read()
@@ -16,9 +23,11 @@ def main():
         img = cv2.resize(img, (1100, 720))
         imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         
-        # Definindo a região de interesse (ROI)
-        x, y, w, h = 500, 430, 30, 290
-        imgTh = cv2.adaptiveThreshold(imgGray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 12)
+        # Aplica o subtrator de fundo
+        fgMask = backSub.apply(imgGray)
+
+        # Aplica limiarização e dilatação para melhorar a detecção
+        _, imgTh = cv2.threshold(fgMask, 200, 255, cv2.THRESH_BINARY)
         kernel = np.ones((8, 8), np.uint8)
         imgDil = cv2.dilate(imgTh, kernel, iterations=2)
 
@@ -34,8 +43,9 @@ def main():
 
         # Desenho de retângulos e informações no frame
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0) if liberado else (255, 0, 255), 4)
-        cv2.rectangle(imgTh, (x, y), (x + w, y + h), (255, 255, 255), 6)
-        cv2.putText(img, str(brancos), (x - 30, y - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+      #  cv2.rectangle(imgTh, (x, y), (x + w, y + h), (255, 255, 255), 6)
+      #  cv2.putText(img, f'Brancos: {brancos}', (x - 30, y - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+      #  cv2.putText(img, f'Pixels: {total_pixels}', (x - 30, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
         cv2.rectangle(img, (575, 155), (575 + 88, 155 + 85), (255, 255, 255), -1)
         cv2.putText(img, str(contador), (x + 100, y), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 5)
         
@@ -54,9 +64,6 @@ def main():
     # Libera o vídeo e fecha todas as janelas abertas do OpenCV
     video.release()
     cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
 
 if __name__ == "__main__":
     main()
